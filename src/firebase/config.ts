82be346a1826f261b02b3a8bc7ebbbcd4da73ae9
Benni <benniboy827@gmail.com>
2026-04-15 11:34:40 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,11 @@
 // This file is intentionally left blank.
 // The Firebase configuration will be populated automatically by Firebase App Hosting.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  apiKey: 'AIzaSyADOwBtYw52FamhtLBD_UOqTcPjlqaftW0',
+  appId: '1:29178779060:web:999f65a5c709432ae24ee1',
+  authDomain: 'studio-5698864488-20bd2.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '29178779060',
+  projectId: 'studio-5698864488-20bd2',
+  storageBucket: 'studio-5698864488-20bd2.appspot.com',
+};
